8df9c2eb50a2d960b3dc87ba57b350134043ea50
Jan <jplosa@tlsi.es>
2022-08-24 15:37:50 +0200

    Fin sección 10 - Auth

--- index.js
+++ index.js
@@ -11,22 +11,19 @@ const app = express();
 // Configurar CORS
 app.use( cors() );
 
+// Lectura y parseo del body
+app.use( express.json() );
+
 // Base de datos
 dbConnection();
 
 //Rutas
-app.get( '/', (req, res) => {
-
-    res.json({
-        ok: true,
-        msg: 'Wololo'
-    })
-
-});
+app.use( '/api/usuarios', require('./routes/usuarios') );
+app.use( '/api/login', require('./routes/auth') );
 
 
 app.listen( process.env.PORT, () => {
-    console.log('Servidor corriendo en puerto ' + 3000 );
+    console.log('Servidor corriendo en puerto ' + process.env.PORT );
 });
 
 
